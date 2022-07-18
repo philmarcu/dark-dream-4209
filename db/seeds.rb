1f1fa345ba997d0666d1e4b1d94b5bc2373414a6
philmarcu <philmark18@gmail.com>
2022-07-18 12:37:34 -0600

    Finish user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+burrito = Recipe.create!(name: "Breakfast Bean Burrito", complexity: 3, genre: "Mexican")
+omelet = Recipe.create!(name: "Cheese Omelet", complexity: 2, genre: "American")
+bacon = Recipe.create!(name: "Bacon Pancake", complexity: 4, genre: "American")
+pho = Recipe.create!(name: "Bun Bo Hue", complexity: 8, genre: "Vietnamese")
+tuna = Recipe.create!(name: "Tuna Roll", complexity: 7, genre: "Japanese")
+pizza = Recipe.create!(name: "Sausage & Pepperoni Pizza", complexity: 5, genre: "American")
---
+++ spec/features/recipes/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipe Index Page' do
+  describe 'user story 1' do
+    context 'it lists recipes' do
+      it 'links to a recipe page' do
+        burrito = Recipe.create!(name: "Breakfast Bean Burrito", complexity: 3, genre: "Mexican")
+        omelet = Recipe.create!(name: "Cheese Omelet", complexity: 2, genre: "American")
+
+        visit "/recipes"
+                
+        within "#recipe-0" do
+          expect(page).to have_content("Breakfast Bean Burrito")
+          expect(page).to have_content("Mexican")
+        end 
+        
+        within "#recipe-1" do
+          expect(page).to have_content("Cheese Omelet")
+          expect(page).to have_content("American")
+        end
+      end
+    end
+
+
+  end
+end
